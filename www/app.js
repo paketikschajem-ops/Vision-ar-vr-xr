97245862059ffccccb7bd7ffc68eeb6bf985e11a
paketikschajem-ops <paketik.s.chajem@gmail.com>
2026-05-20 18:00:26 +0300

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,36 +1,33 @@
 let scene, camera, renderer;
 
-init();
+scene = new THREE.Scene();
 
-function init() {
-  scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
+  70,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  100
+);
 
-  camera = new THREE.PerspectiveCamera(
-    70,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    100
-  );
+renderer = new THREE.WebGLRenderer({
+  canvas: document.getElementById("scene"),
+  alpha: true
+});
 
-  renderer = new THREE.WebGLRenderer({
-    canvas: document.getElementById("scene"),
-    alpha: true
-  });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(0.3, 0.3, 0.3),
+  new THREE.MeshNormalMaterial()
+);
 
-  const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(0.3, 0.3, 0.3),
-    new THREE.MeshNormalMaterial()
-  );
-
-  cube.position.z = -1;
-  scene.add(cube);
-
-  animate();
-}
+cube.position.z = -1.5;
+scene.add(cube);
 
 function animate() {
   requestAnimationFrame(animate);
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
+
+animate();
