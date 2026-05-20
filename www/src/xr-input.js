7602638063898ff77d33d43a67e1c634b75054e2
paketikschajem-ops <paketik.s.chajem@gmail.com>
2026-05-20 20:29:20 +0300

    Update xr-input.js

--- www/src/xr-input.js
+++ www/src/xr-input.js
@@ -1,1 +1,19 @@
+let head = { x: 0, y: 0, z: 0 };
 
+window.addEventListener("deviceorientation", (e) => {
+  if (!e.alpha) return;
+
+  head.x = e.alpha;
+  head.y = e.beta;
+  head.z = e.gamma;
+});
+
+function lerp(a, b, t) {
+  return a + (b - a) * t;
+}
+
+function updateGyro(camera) {
+  camera.rotation.y = lerp(camera.rotation.y, head.x * Math.PI / 180, 0.08);
+  camera.rotation.x = lerp(camera.rotation.x, head.y * Math.PI / 180, 0.08);
+  camera.rotation.z = lerp(camera.rotation.z, head.z * Math.PI / 180, 0.08);
+}
